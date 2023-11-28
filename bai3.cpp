87bb0b5aa67c331b8b1f00051da7f3d9be01950b
nguyenminhhuy12 <151911015+nguyenminhhuy12@users.noreply.github.com>
2023-11-28 15:32:57 +0700

    Add files via upload

--- bai3.cpp
+++ bai3.cpp
@@ -1,99 +1,74 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <climits>
+#include <string>
 
 using namespace std;
 
-class Query {
-private:
-    int u, v;
-
+class TimBieuThuc {
 public:
-    Query(int uu, int vv) : u(uu), v(vv) {}
-
-    int getU() const {
-        return u;
-    }
-
-    int getV() const {
-        return v;
-    }
-};
-
-class Update {
-private:
-    int u, v, k;
-
-public:
-    Update(int uu, int vv, int kk) : u(uu), v(vv), k(kk) {}
-
-    int getU() const {
-        return u;
-    }
-
-    int getV() const {
-        return v;
-    }
-
-    int getK() const {
-        return k;
-    }
-};
-
-class ArrayUpdater {
-private:
-    vector<int> arr;
-
-public:
-    ArrayUpdater(int n) : arr(n, 0) {}
-
-    void applyUpdate(const Update& update) {
-        arr[update.getU() - 1] += update.getK();
-        if (update.getV() < arr.size()) {
-            arr[update.getV()] -= update.getK();
+    string timBieuThuc(vector<long long>& daySo, long long tongMucTieu) {
+        string toanTu = "-~+.";
+        string ketQua;
+        if (taoBieuThuc(daySo, tongMucTieu, 0, "", toanTu, ketQua)) {
+            return ketQua;
+        } else {
+            return "impossible";
         }
     }
 
-    void calculatePrefixSum() {
-        for (int i = 1; i < arr.size(); ++i) {
-            arr[i] += arr[i - 1];
+private:
+    bool tinhBieuThuc(vector<long long>& daySo, string& bieuThuc, long long tongMucTieu) {
+        long long ketQua = daySo[0];
+        int j = 0;
+        for (int i = 0; i < bieuThuc.length(); ++i) {
+            if (bieuThuc[i] == '+') {
+                ketQua += daySo[j + 1];
+            } else if (bieuThuc[i] == '-') {
+                ketQua -= daySo[j + 1];
+            } else if (bieuThuc[i] == '~') {
+                ketQua = -daySo[j + 1];
+            } else if (bieuThuc[i] == '.') {
+                ketQua = ketQua * 10 + daySo[j + 1];
+            }
+            j++;
         }
+        return ketQua == tongMucTieu;
     }
 
-    int getMaxValueInRange(int u, int v) const {
-        int maxVal = INT_MIN;
-        for (int i = u - 1; i < v; ++i) {
-            maxVal = max(maxVal, arr[i]);
+    bool taoBieuThuc(vector<long long>& daySo, long long tongMucTieu, int viTriHienTai, string bieuThucHienTai, string& toanTu, string& ketQua) {
+        if (viTriHienTai == daySo.size() - 1) {
+            if (tinhBieuThuc(daySo, bieuThucHienTai, tongMucTieu)) {
+                ketQua = bieuThucHienTai;
+                return true;
+            }
+            return false;
         }
-        return maxVal;
+
+        for (int i = 0; i < toanTu.length(); ++i) {
+            string bieuThucMoi = bieuThucHienTai + toanTu[i];
+            if (taoBieuThuc(daySo, tongMucTieu, viTriHienTai + 1, bieuThucMoi, toanTu, ketQua)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 };
 
 int main() {
-    int n, m;
-    cin >> n >> m;
+    int N;
+    long long S;
+    cin >> N >> S;
 
-    ArrayUpdater arrayUpdater(n);
-
-    for (int i = 0; i < m; ++i) {
-        int u, v, k;
-        cin >> u >> v >> k;
-        Update update(u, v, k);
-        arrayUpdater.applyUpdate(update);
+    vector<long long> daySo(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> daySo[i];
     }
 
-    arrayUpdater.calculatePrefixSum();
+    TimBieuThuc timKiem;
+    string ketQua = timKiem.timBieuThuc(daySo, S);
 
-    int p;
-    cin >> p;
-
-    for (int i = 0; i < p; ++i) {
-        int u, v;
-        cin >> u >> v;
-        Query query(u, v);
-        cout << arrayUpdater.getMaxValueInRange(query.getU(), query.getV()) << endl;
-    }
+    cout << ketQua << endl;
 
     return 0;
 }
